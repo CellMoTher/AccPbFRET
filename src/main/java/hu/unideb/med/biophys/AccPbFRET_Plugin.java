b53b4047588a3d8b15862c73e5f4831d2e796ac7
Cameron Lloyd <lloyd@med.unideb.hu>
2019-06-24 21:27:11 +0200

    Update license header

--- src/main/java/hu/unideb/med/biophys/AccPbFRET_Plugin.java
+++ src/main/java/hu/unideb/med/biophys/AccPbFRET_Plugin.java
@@ -1,13 +1,19 @@
-/*
-AccPbFRET - an ImageJ plugin for analysis of acceptor photobleaching FRET images.
-
-Written by Janos Roszik (janosr@med.unideb.hu), Janos Szollosi (szollo@med.unideb.hu) and Gyorgy Vereb (vereb@med.unideb.hu)
-
-The program is provided free of charge on an "as is" basis without warranty of any kind.
-
-http://biophys.med.unideb.hu/accpbfret/
-*/
-
+/* 
+ * Copyright (C) 2008 - 2019 AccPbFRET developers.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package hu.unideb.med.biophys;
 
 import ij.*;
